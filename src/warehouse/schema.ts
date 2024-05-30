fa1cfd1c42cb5d317219a766c63196ac8fcb0b19
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-05-30 15:07:32 +0700

    add updateWarehouseSchema schema

--- src/warehouse/schema.ts
+++ src/warehouse/schema.ts
@@ -15,4 +15,14 @@ export const createWarehouseSchema = z.object({
     .min(1, 'warehouse address is required'),
 });
 
+export const updateWarehouseSchema = z.object({
+  name: z
+    .string({ required_error: 'warehouse name is required' })
+    .min(1, 'warehouse name is required'),
+  address: z
+    .string({ required_error: 'warehouse address is required' })
+    .min(1, 'warehouse address is required'),
+});
+
 export type CreateWarehouse = z.TypeOf<typeof createWarehouseSchema>;
+export type UpdateWarehouse = z.TypeOf<typeof updateWarehouseSchema>;
