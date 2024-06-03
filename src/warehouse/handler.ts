f99a83f6fb420739be78ac6e44da7b0ba4d0a8b2
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-06-03 18:59:40 +0700

    add getStocksFromAllWarehouses handler

--- src/warehouse/handler.ts
+++ src/warehouse/handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import {
   deleteWarehouseById,
+  findStocksFromAllWarehouses,
   findStocksFromWarehouse,
   findWarehouseById,
   findWarehouses,
@@ -157,5 +158,51 @@ export async function deleteWarehouse(
     return res
       .status(400)
       .json({ status: 'fail', errors: { message: error.message, code: 400 } });
+  }
+}
+
+export async function getStocksFromAllWarehouses(
+  req: Request,
+  res: Response<ApiResponse, CurrentUser>,
+) {
+  try {
+    const result = await findStocksFromAllWarehouses(res.locals.user.userId);
+    const warehouseMap = new Map();
+
+    result.forEach((item) => {
+      const warehouse = {
+        id: item.warehouse_id,
+        name: item.warehouse_name,
+        address: item.warehouse_address,
+      };
+      const stock = {
+        id: item.id,
+        name: item.name,
+        supplier: item.supplier,
+        quantity: item.quantity,
+        cost_price: item.cost_price,
+        purchase_date: item.purchase_date,
+        stock_due_date: item.stock_due_date,
+        created_at: item.created_at,
+        updated_at: item.updated_at,
+        amount: item.cost_price * item.quantity,
+      };
+
+      if (!warehouseMap.has(item.warehouse_id)) {
+        warehouseMap.set(item.warehouse_id, { ...warehouse, stocks: [] });
+      }
+
+      warehouseMap.get(item.warehouse_id).stocks.push(stock);
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      data: { warehouses: Array.from(warehouseMap.values()) },
+    });
+  } catch (error: any) {
+    console.log('get_stocks_from_all_warehouses', error);
+    return res
+      .status(404)
+      .json({ status: 'fail', errors: { message: error.message, code: 404 } });
   }
 }
