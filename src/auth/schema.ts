738f0e1b8d2bed738c67e117486796dcf2c84d6e
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-06-23 14:31:01 +0700

    create login type based on zod schema

--- src/auth/schema.ts
+++ src/auth/schema.ts
@@ -8,3 +8,5 @@ export const loginSchema = z.object({
     .string({ required_error: 'password is required' })
     .min(1, 'password is required'),
 });
+
+export type Login = z.TypeOf<typeof loginSchema>;
