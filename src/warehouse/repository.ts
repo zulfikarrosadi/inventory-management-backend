67051c18fd64497c3290c04d8bf0c0cffc33e7f3
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-09-05 12:43:40 +0700

    refactor warehouse repo to be class based to make DI easier

--- src/warehouse/repository.ts
+++ src/warehouse/repository.ts
@@ -1,111 +1,119 @@
-import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { CreateWarehouse, UpdateWarehouse, Warehouse } from './schema';
-import pool from '../db';
+import { NotFoundError } from '../lib/Error';
+import { Stock } from '../inventory/schema';
 
-export async function saveWarehouse(
-  data: CreateWarehouse,
-  userId: number,
-): Promise<ResultSetHeader | Error> {
-  try {
-    const [rows] = await pool.execute(
-      'INSERT INTO warehouses (name, address, user_id) VALUES (?,?,?)',
+type StockFromWarehouse = Array<
+  | {
+      warehouse_id: number;
+      warehouse_name: string;
+      warehouse_address: string;
+    } & Stock
+>;
+
+class WarehouseRepository {
+  constructor(private db: Pool) {}
+
+  async saveWarehouse(data: CreateWarehouse, userId: number) {
+    const [rows] = await this.db.execute(
+      'INSERT INTO warehouses (name, address, user_id) VALUES (?, ?, ?)',
       [data.name, data.address, userId],
     );
-
-    return rows as unknown as ResultSetHeader;
-  } catch (error: any) {
-    console.log('save_warehouse', error);
-
-    return error;
-  }
-}
-
-export async function findWarehouses(
-  userId: number,
-): Promise<Warehouse[] | Error> {
-  const [rows] = await pool.query<RowDataPacket[]>(
-    'SELECT warehouses.id, warehouses.name, warehouses.address FROM warehouses JOIN users ON warehouses.user_id = users.id WHERE users.id = ? LIMIT 50',
-    [userId],
-  );
-  if (!rows.length) {
-    throw new Error('no warehouse found, create new warehouse to start');
+    if (!rows) {
+      console.log('save warehouse error', rows);
+      throw new Error('something went wrong, please try again');
+    }
+    const result = rows as ResultSetHeader;
+    return { warehouseId: result.insertId };
   }
 
-  return rows as unknown as Warehouse[];
-}
-
-export async function updateWarehouseById(
-  id: number,
-  userId: number,
-  data: UpdateWarehouse,
-): Promise<ResultSetHeader | Error> {
-  const [rows] = await pool.execute(
-    'UPDATE warehouses SET name = ?, address = ? WHERE id = ? AND user_id = ?',
-    [data.name, data.address, id, userId],
-  );
-  const result = rows as ResultSetHeader;
-
-  if (result.affectedRows === 0) {
-    throw new Error(
-      'failed to update warehouse, please enter correct data and try again',
+  async findWarehouseById(warehouseId: number, userId: number) {
+    const [rows] = await this.db.query<RowDataPacket[]>(
+      'SELECT id, name, address, user_id from warehouses where id = ? AND user_id = ?',
+      [warehouseId, userId],
     );
+
+    if (!rows) {
+      throw new NotFoundError(
+        'no warehouse found, enter correct info and try again',
+      );
+    }
+    return rows[0] as unknown as Warehouse;
   }
 
-  return result;
-}
-
-export async function findWarehouseById(
-  id: number,
-  userId: number,
-): Promise<Warehouse> {
-  const [rows] = await pool.query<RowDataPacket[]>(
-    'SELECT id, name, address FROM warehouses WHERE id = ? AND user_id = ?',
-    [id, userId],
-  );
-  if (!rows.length) {
-    throw new Error(
-      'warehouse not found, please enter the correct id and try again',
+  async findWarehouses(userId: number) {
+    const [rows] = await this.db.query<RowDataPacket[]>(
+      'SELECT warehouses.id, warehouses.name, warehouses.address FROM warehouses JOIN users ON warehouses.user_id = users.id WHERE users.id = ? LIMIT 50',
+      [userId],
     );
+    if (!rows.length) {
+      throw new NotFoundError(
+        'no warehouse found, create new warehouse to start',
+      );
+    }
+
+    return rows as unknown as Warehouse[];
   }
 
-  return rows[0] as unknown as Warehouse;
-}
-
-export async function findStocksFromWarehouse(
-  warehouse_id: number,
-  userId: number,
-) {
-  const [rows] = await pool.query<RowDataPacket[]>(
-    `SELECT warehouses.id AS warehouse_id, warehouses.name AS warehouse_name, warehouses.address AS warehouse_address, stocks.id, stocks.name, stocks.supplier, stocks.quantity, stocks.cost_price, stocks.purchase_date, stocks.stock_due_date, stocks.created_at, stocks.updated_at FROM warehouses JOIN stocks ON stocks.warehouse_id = warehouses.id WHERE warehouses.id = ? AND warehouses.user_id = ?`,
-    [warehouse_id, userId],
-  );
-  if (!rows.length) {
-    throw new Error('no stocks in this warehouse is found');
+  async updateWarehouseById(
+    warehouseId: number,
+    userId: number,
+    data: UpdateWarehouse,
+  ) {
+    const [rows] = await this.db.execute(
+      'UPDATE warehouses SET name = ?, address = ? WHERE id = ? AND user_id = ?',
+      [data.name, data.address, warehouseId, userId],
+    );
+    const result = rows as ResultSetHeader;
+    if (!result) {
+      console.log('update warehouse by id error', result);
+      throw new Error(
+        'something went wrong, enter correct data and please try again',
+      );
+    }
+    return { affectedRows: result.affectedRows };
   }
 
-  return rows;
-}
-
-export async function deleteWarehouseById(id: number, userId: number) {
-  const [rows] = await pool.execute(
-    'DELETE FROM warehouses WHERE id = ? AND user_id = ?',
-    [id, userId],
-  );
-  const result = rows as ResultSetHeader;
-
-  if (result.affectedRows === 0) {
-    throw new Error('delete fail, enter the correct id and try again');
+  async findStockFromWarehouse(warehouseId: number, userId: number) {
+    const [rows] = await this.db.query<RowDataPacket[]>(
+      `SELECT warehouses.id AS warehouse_id, warehouses.name AS warehouse_name, warehouses.address AS warehouse_address, stocks.id, stocks.name, stocks.supplier, stocks.quantity, stocks.cost_price, stocks.purchase_date, stocks.stock_due_date, stocks.created_at, stocks.updated_at FROM warehouses JOIN stocks ON stocks.warehouse_id = warehouses.id WHERE warehouses.id = ? AND warehouses.user_id = ?`,
+      [warehouseId, userId],
+    );
+    if (!rows.length) {
+      throw new NotFoundError('this warehouse is empty');
+    }
+    return rows as unknown as Stock[];
   }
-  return true;
+
+  async deleteWarehouseById(warehouseId: number, userId: number) {
+    const [rows] = await this.db.execute(
+      'DELETE FROM warehouses WHERE id = ? AND user_id = ?',
+      [warehouseId, userId],
+    );
+
+    const result = rows as ResultSetHeader;
+    if (result.affectedRows === 0) {
+      throw new Error(
+        'fail to delete this warehouse, please enter the correct information and try again',
+      );
+    }
+
+    return { affectedRows: result.affectedRows };
+  }
+
+  async findStocksFromAllWarehouses(
+    userId: number,
+  ): Promise<StockFromWarehouse> {
+    const [rows] = await this.db.query<RowDataPacket[]>(
+      'SELECT warehouses.id AS warehouse_id, warehouses.name AS warehouse_name, warehouses.address AS warehouse_address, stocks.id, stocks.name, stocks.supplier, stocks.quantity, stocks.cost_price, stocks.purchase_date, stocks.stock_due_date, stocks.created_at, stocks.updated_at FROM warehouses JOIN stocks ON stocks.warehouse_id = warehouses.id WHERE warehouses.user_id = ?',
+      [userId],
+    );
+    if (!rows.length) {
+      throw new Error('no warehouse and stocks found');
+    }
+    const result = rows as unknown as StockFromWarehouse;
+    return result;
+  }
 }
 
-export async function findStocksFromAllWarehouses(userId: number) {
-  const [rows] = await pool.query<RowDataPacket[]>(
-    'SELECT warehouses.id AS warehouse_id, warehouses.name AS warehouse_name, warehouses.address AS warehouse_address, stocks.id, stocks.name, stocks.supplier, stocks.quantity, stocks.cost_price, stocks.purchase_date, stocks.stock_due_date, stocks.created_at, stocks.updated_at FROM warehouses JOIN stocks ON stocks.warehouse_id = warehouses.id WHERE warehouses.user_id = ?',
-    [userId],
-  );
-  if (!rows.length) {
-    throw new Error('no warehouse and stocks found');
-  }
-  return rows;
-}
+export default WarehouseRepository;
