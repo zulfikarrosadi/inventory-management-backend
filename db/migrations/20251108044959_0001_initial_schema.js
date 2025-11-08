1c2ddcb515338127b7444c7705890308618066b5
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-11-08 11:59:47 +0700

    feat: create db migration

---
+++ db/migrations/20251108044959_0001_initial_schema.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+	return Promise.resolve();
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+	return Promise.resolve();
+};
---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+require('dotenv').config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
+    },
+    migrations: {
+      directory: './db/migrations'
+    }
+  },
+};
