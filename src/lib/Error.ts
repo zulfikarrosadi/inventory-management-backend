3c4ac6cce483e391172c4dcb995dcc8dfa92e040
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-11-10 15:28:08 +0700

    feat: create custom error
    to make it easier to give user response if some error happen

--- src/lib/Error.ts
+++ src/lib/Error.ts
@@ -1,17 +1,37 @@
-export class AuthCredentialError extends Error {
-  constructor(message: string = 'username or password is incorrect') {
+export class AppError extends Error {
+  constructor(public code: number, public message: string) {
     super(message);
+    this.code = code
   }
 }
 
-export class UsernameAlreadyExistsError extends Error {
-  constructor(message: string = 'this username already exists') {
-    super(message);
+export class AuthCredentialError extends AppError {
+  constructor(message: string = 'username or password is incorrect', code: number = 401) {
+    super(code, message);
   }
 }
 
-export class NotFoundError extends Error {
+export class UsernameAlreadyExistsError extends AppError {
+  constructor(message: string = 'this username already exists', code: number = 409) {
+    super(code, message);
+  }
+}
+
+export class BadRequestError extends AppError {
   constructor(message: string) {
-    super(message);
+    super(400, message);
+  }
+}
+
+
+export class ConflictError extends AppError {
+  constructor(message: string) {
+    super(409, message);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string) {
+    super(404, message);
   }
 }
