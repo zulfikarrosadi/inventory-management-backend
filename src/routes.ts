032da197d71e20b10233a3d6f96017b98c97836a
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-11-14 13:53:01 +0700

    chor: format code

--- src/routes.ts
+++ src/routes.ts
@@ -1,30 +1,38 @@
-import { Express } from 'express';
-import { validateInput } from './middlewares/validateInput';
-import { createUserSchema } from './user/schema';
-import { loginSchema } from './auth/schema';
-import { deserializeToken } from './middlewares/deserializeToken';
-import requiredLogin from './middlewares/requiredLogin';
-import { createStockSchema, updateStockQuantitySchema, updateStockSchema } from './inventory/schema';
-import { createWarehouseSchema } from './warehouse/schema';
-import AuthRepository from './auth/repository';
-import connection from './db';
-import AuthService from './auth/service';
-import AuthHandler from './auth/handler';
-import UserRepository from './user/repository';
-import UserSerivce from './user/service';
-import UserHandler from './user/handler';
-import sanitizeInput from './middlewares/sanitizeInput';
-import WarehouseRepository from './warehouse/repository';
-import WarehosueService from './warehouse/service';
-import WarehouseHandler from './warehouse/handler';
-import InventoryService from './inventory/service';
-import InventoryRepository from './inventory/repository';
-import InventoryHandler from './inventory/handler';
-import { logRequest } from './middlewares/logger';
-import { requestId } from './middlewares/requestId';
-import { logger } from './lib/logger';
-import { setContext } from './middlewares/context';
-import { catchAllError } from './middlewares/catchError';
+import type { Express } from "express";
+import AuthHandler from "./auth/handler";
+import AuthRepository from "./auth/repository";
+import { loginSchema } from "./auth/schema";
+import AuthService from "./auth/service";
+import connection from "./db";
+import InventoryHandler from "./inventory/handler";
+import InventoryRepository from "./inventory/repository";
+import {
+  createStockSchema,
+  updateStockQuantitySchema,
+  updateStockSchema,
+} from "./inventory/schema";
+import InventoryService from "./inventory/service";
+import { logger } from "./lib/logger";
+import { catchAllError } from "./middlewares/catchError";
+import { setContext } from "./middlewares/context";
+import { deserializeToken } from "./middlewares/deserializeToken";
+import { logRequest } from "./middlewares/logger";
+import { requestId } from "./middlewares/requestId";
+import requiredLogin from "./middlewares/requiredLogin";
+import sanitizeInput from "./middlewares/sanitizeInput";
+import { validateInput } from "./middlewares/validateInput";
+import OrgHandler from "./organzation/handler";
+import OrgRepository from "./organzation/repository";
+import OrgService from "./organzation/service";
+import UserHandler from "./user/handler";
+import UserRepository from "./user/repository";
+import { createUserSchema } from "./user/schema";
+import UserSerivce from "./user/service";
+import WarehouseHandler from "./warehouse/handler";
+import WarehouseRepository from "./warehouse/repository";
+import { createWarehouseSchema } from "./warehouse/schema";
+import WarehosueService from "./warehouse/service";
+import { createOrgSchema, updateOrgSchema } from "./organzation/schema";
 
 export default function routes(app: Express) {
   const authRepo = new AuthRepository(connection);
@@ -39,48 +47,65 @@ export default function routes(app: Express) {
   const warehouseService = new WarehosueService(warehosueRepo);
   const warehouseHandler = new WarehouseHandler(warehouseService);
 
-  const inventoryRepo = new InventoryRepository(connection, logger)
-  const inventoryService = new InventoryService(inventoryRepo, warehosueRepo)
-  const inventoryHandler = new InventoryHandler(inventoryService)
+  const inventoryRepo = new InventoryRepository(connection, logger);
+  const inventoryService = new InventoryService(inventoryRepo, warehosueRepo);
+  const inventoryHandler = new InventoryHandler(inventoryService);
+
+  const orgRepo = new OrgRepository(connection, logger);
+  const orgService = new OrgService(orgRepo, logger);
+  const orgHandler = new OrgHandler(orgService);
 
   app.use(sanitizeInput);
 
-  app.use(requestId)
-  app.use(setContext)
-  app.use(logRequest)
+  app.use(requestId);
+  app.use(setContext);
+  app.use(logRequest);
   app.post(
-    '/api/register',
-    //@ts-ignore
+    "/api/register",
+    //@ts-expect-error
     validateInput(createUserSchema),
     userHandler.registerUser,
   );
-  app.post('/api/login', validateInput(loginSchema), authHandler.login);
-  app.get('/api/refresh', authHandler.refreshToken);
+  app.post("/api/login", validateInput(loginSchema), authHandler.login);
+  app.get("/api/refresh", authHandler.refreshToken);
 
   app.use(deserializeToken);
   app.use(requiredLogin);
-  app.get('/api/users', userHandler.getCurrentUser);
-  app.get('/api/users/:id', userHandler.getUserById);
+  app.get("/api/users", userHandler.getCurrentUser);
+  app.get("/api/users/:id", userHandler.getUserById);
 
-  app.post('/api/stocks', validateInput(createStockSchema), inventoryHandler.createStock);
-  app.get('/api/stocks/:id', inventoryHandler.getStockById);
-  app.put('/api/stocks/:id', validateInput(updateStockSchema), inventoryHandler.updateStock);
-  app.delete('/api/stocks/:id', inventoryHandler.deleteStock);
-  app.patch('/api/stocks/:id', validateInput(updateStockQuantitySchema), inventoryHandler.updateStockQuantity)
 
   app.post(
-    '/api/warehouses',
+    "/api/stocks",
+    validateInput(createStockSchema),
+    inventoryHandler.createStock,
+  );
+  app.get("/api/stocks/:id", inventoryHandler.getStockById);
+  app.put(
+    "/api/stocks/:id",
+    validateInput(updateStockSchema),
+    inventoryHandler.updateStock,
+  );
+  app.delete("/api/stocks/:id", inventoryHandler.deleteStock);
+  app.patch(
+    "/api/stocks/:id",
+    validateInput(updateStockQuantitySchema),
+    inventoryHandler.updateStockQuantity,
+  );
+
+  app.post(
+    "/api/warehouses",
     validateInput(createWarehouseSchema),
     warehouseHandler.createWarehouse,
   );
-  app.put('/api/warehouses/:id', warehouseHandler.updateWarehouse);
-  app.get('/api/warehouses/:id/stocks', warehouseHandler.getStockFromWarehouse);
-  app.get('/api/warehouses', warehouseHandler.getWarehouses);
-  app.delete('/api/warehouses/:id', warehouseHandler.deleteWarehouse);
+  app.put("/api/warehouses/:id", warehouseHandler.updateWarehouse);
+  app.get("/api/warehouses/:id/stocks", warehouseHandler.getStockFromWarehouse);
+  app.get("/api/warehouses", warehouseHandler.getWarehouses);
+  app.delete("/api/warehouses/:id", warehouseHandler.deleteWarehouse);
   app.get(
-    '/api/warehouses/stocks',
+    "/api/warehouses/stocks",
     warehouseHandler.getStocksFromAllWarehouses,
   );
 
-  app.use(catchAllError)
+  app.use(catchAllError);
 }
